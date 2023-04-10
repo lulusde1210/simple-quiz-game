9e78fb9300c9a622943de05b0b2b7ec32219eb6f
lulu <lulusun1210@gmail.com>
2023-04-10 13:31:33 -0500

    update the question data by sending request to trivia API

--- quiz_game/quiz_brain.py
+++ quiz_game/quiz_brain.py
@@ -1,19 +1,23 @@
+import html
+
+
 class QuizBrain:
-    def __init__(self, question_list):
+    def __init__(self, question_bank):
         self.question_number = 0
-        self.question_list = question_list
+        self.question_bank = question_bank
         self.score = 0
 
     def still_has_questions(self):
         """check if there is still questions left"""
-        return self.question_number < len(self.question_list)
+        return self.question_number < len(self.question_bank)
 
     def ask_question(self):
         """prompt user to answer questions."""
-        current_question = self.question_list[self.question_number]
+        current_question = self.question_bank[self.question_number]
         self.question_number += 1
+        question_text = html.unescape(current_question.text)
         user_answer = input(
-            f"Q.{self.question_number}:{current_question.text} (True/False):")
+            f"Q.{self.question_number}:{question_text} (True/False):")
         self.check_answer(user_answer, current_question.answer)
         self.display_score()
         print("\n")
--- quiz_game/quiz_manager.py
+++ quiz_game/quiz_manager.py
@@ -6,19 +6,10 @@ class QuizManager:
     def __init__(self, question_data):
         self.question_data = question_data
 
-    def choose_questions_from_data(self):
-        """Chose 10 random questions from question data"""
-        random_questions = []
-        for _ in range(10):
-            random_index = random.randrange(0, len(self.question_data))
-            random_question = self.question_data[random_index]
-            random_questions.append(random_question)
-        return random_questions
-
     def create_question_bank(self):
         """Create a question bank from question data"""
         question_bank = []
-        for question in self.choose_questions_from_data():
+        for question in self.question_data:
             text = question["question"]
             answer = question["correct_answer"]
             new_question = Question(text, answer)
